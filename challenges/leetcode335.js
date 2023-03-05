fc93ff61f4f07e4e10ff154dd22cade94b85b19f
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2023-03-04 21:34:43 -0700

    copied remainder of questions to solve later

--- challenges/leetcode335.js
+++ challenges/leetcode335.js
@@ -104,3 +104,44 @@ var kthLargestLevelSum = function(root, k) {
     return -1;
   }
 };
+
+/*
+  You are given a 0-indexed integer array nums of length n.
+
+A split at an index i where 0 <= i <= n - 2 is called valid if the product of the first i + 1 elements and the product of the remaining elements are coprime.
+
+For example, if nums = [2, 3, 3], then a split at the index i = 0 is valid because 2 and 9 are coprime, while a split at the index i = 1 is not valid because 6 and 3 are not coprime. A split at the index i = 2 is not valid because i == n - 1.
+Return the smallest index i at which the array can be split validly or -1 if there is no such split.
+
+Two values val1 and val2 are coprime if gcd(val1, val2) == 1 where gcd(val1, val2) is the greatest common divisor of val1 and val2.
+  Input: nums = [4,7,8,15,3,5]
+  Output: 2
+
+  Input: nums = [4,7,15,8,3,5]
+  Output: -1
+*/
+
+var findValidSplit = function(nums) {
+
+};
+
+/*
+There is a test that has n types of questions. You are given an integer target and a 0-indexed 2D integer array types where types[i] = [counti, marksi] indicates that there are counti questions of the ith type, and each one of them is worth marksi points.
+
+Return the number of ways you can earn exactly target points in the exam. Since the answer may be too large, return it modulo 109 + 7.
+
+Note that questions of the same type are indistinguishable.
+
+For example, if there are 3 questions of the same type, then solving the 1st and 2nd questions is the same as solving the 1st and 3rd questions, or the 2nd and 3rd questions.
+
+Input: target = 6, types = [[6,1],[3,2],[2,3]]
+Output: 7
+Input: target = 5, types = [[50,1],[50,2],[50,5]]
+Output: 4
+Input: target = 18, types = [[6,1],[3,2],[2,3]]
+Output: 1
+*/
+
+var waysToReachTarget = function(target, types) {
+
+};
