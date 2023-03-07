ccd6601431e03dfdfd135bca6f68119cc7a20462
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2023-03-07 14:18:52 -0700

    Saving here for stream

---
+++ challenges/minimum-time.js
@@ -0,0 +1,15 @@
+/*
+You are given an array time where time[i] denotes the time taken by the ith bus to complete one trip.
+
+Each bus can make multiple trips successively; that is, the next trip can start immediately after completing the current trip. Also, each bus operates independently; that is, the trips of one bus do not influence the trips of any other bus.
+
+You are also given an integer totalTrips, which denotes the number of trips all buses should make in total. Return the minimum time required for all buses to complete at least totalTrips trips.
+*/
+
+var minimumTime = function(time, totalTrips) {
+
+  let tripCount = 0;
+
+  return tripCount;
+
+};
